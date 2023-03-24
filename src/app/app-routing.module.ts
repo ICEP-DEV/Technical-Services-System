de561a42db0388480ea109f3d2552e21f2f59b70
unknown <neotshabalalamof5@gmail.com>
2023-03-24 11:56:58 +0200

    added side

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ViewrequestComponent } from './pages/viewrequest/viewrequest.component';
+import { ComponentComponent } from './component/component.component';
 
 
 const routes: Routes = [
 
     {path:'', component:HomeComponent},
-    {path:'viewrequest', component:ViewrequestComponent}
+    {path:'viewrequest', component:ComponentComponent},
+    {path:'component', component:ViewrequestComponent}
 ];
 
 @NgModule({
