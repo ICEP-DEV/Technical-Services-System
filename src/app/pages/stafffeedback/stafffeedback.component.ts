5801788aacccb146a194108ff61cf9b4ab47d723
unknown <neotshabalalamof5@gmail.com>
2023-04-24 15:31:16 +0200

    feedback

---
+++ src/app/pages/stafffeedback/stafffeedback.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stafffeedback',
+  templateUrl: './stafffeedback.component.html',
+  styleUrls: ['./stafffeedback.component.css']
+})
+export class StafffeedbackComponent {
+
+}
