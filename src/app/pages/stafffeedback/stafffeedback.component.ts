51245664b0665803f837c78b0cb048ee8da313e7
unknown <neotshabalalamof5@gmail.com>
2023-05-09 10:48:19 +0200

    added node modules and fixed errors

--- src/app/pages/stafffeedback/stafffeedback.component.ts
+++ src/app/pages/stafffeedback/stafffeedback.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { Data } from '@angular/router';
-import { MyHttpServiceService } from 'src/app/my-http-service.service';
-import { StaffClass } from 'src/app/staff-class';
-
 
 @Component({
   selector: 'app-stafffeedback',
@@ -11,67 +7,4 @@ import { StaffClass } from 'src/app/staff-class';
 })
 export class StafffeedbackComponent {
 
-  
-  option = "";
-  description = "";
-   data = "";
-  id = "";
-
-
-  constructor (private staffServ:MyHttpServiceService) {  
-  }
-
-  
-  errorMsg = ["Both fields are empty","Description is required","3"];
-  submitResults(feedback: any) {
-    console.log('You have clicked the submit button');
-    console.log(feedback.value.inlineRadioOptions);
-    console.log(feedback.value.description);
-
-    if (feedback.value.inlineRadioOptions == "" && feedback.value.description == "") {
-
-        alert(this.errorMsg[0]); 
-    }
-    else 
-    if(feedback.value.inlineRadioOptions == "" )
-    {
-      alert(this.errorMsg[1]); 
-    }
-    else 
-    if(feedback.value.description == "" )
-    {
-      alert(this.errorMsg[1]); 
-    }
-    
-    else {
-      this.option = feedback.value.inlineRadioOptions;
-      this.description = feedback.value.description;
-      this.data = feedback.value
-
-
-    }
-
-    /*this.staffServ.postStaffFeedback(this.data).subscribe((v) => {
-
-        console.log(v);
-
-    });
-    */
-     
-    this.staffServ.getSentRequesData().subscribe((v) => {
-        
-        this.id = v;
-        for (let i = 0; i < 5; i++ ){
-          console.log(this.id[2]);
-        }
-       
-    })
-
-    
-      
-  }
-
 }
-
-
-
