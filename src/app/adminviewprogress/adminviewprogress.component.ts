c01d50802b591b8a4821c8a47a33d018a00fa910
unknown <neotshabalalamof5@gmail.com>
2023-06-05 15:18:37 +0200

    CombineApp

--- src/app/adminviewprogress/adminviewprogress.component.ts
+++ src/app/adminviewprogress/adminviewprogress.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiserviceService } from '../apiservice.service';
+
 
 @Component({
   selector: 'app-adminviewprogress',
@@ -7,6 +10,49 @@ import { Component } from '@angular/core';
 })
 export class AdminviewprogressComponent {
 
+  availableTechData:any;
+
+  data:any;
+  message = "";
+
+  constructor(private service:ApiserviceService,private location: Location, private navrouter:Router){}
+
+
+  ngOnInit(): void{
+ 
+
+
+  let data = localStorage.getItem('reference');
+
+ //let id = data?.slice(7,30);
+ console.log(data);
+  
+    this.service.getTechAvailable(Number(data)).subscribe((res)=>{
+      console.log(res,"res==>");
+      this.availableTechData = res;
+     
+    })
+  }
+
+
+
+  logout(){
+    localStorage.removeItem('logindata')
+  }
+
+  assignArtisan() {
+
+    setTimeout(function(){
+      window.location.reload();
+    }, 3000);
+   
+
+    this.message = "";
+      this.message = "Successfully assigned the request to a artisan, ";
+      return 
+      
+    
+  }
 
   printer(){
     window.print();
