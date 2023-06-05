8c27df68c5ab25559f79d2fea7a8e9fe1789815a
unknown <neotshabalalamof5@gmail.com>
2023-06-05 15:53:42 +0200

    Tomorrow, fifxing the admin prpgress page

--- src/app/adminprogress/adminprogress.component.ts
+++ src/app/adminprogress/adminprogress.component.ts
@@ -14,46 +14,23 @@ export class AdminprogressComponent {
   data:any;
   message = "";
 
-  constructor(private service:ApiserviceService, private navrouter:Router){}
+  constructor(private service:ApiserviceService){}
 
 
   ngOnInit(): void{
+    let data = localStorage.getItem('reference');
+  }
+
  
 
 
-  let data = localStorage.getItem('reference');
-
- //let id = data?.slice(7,30);
- console.log(data);
-  
-    this.service.getTechAvailable(Number(data)).subscribe((res)=>{
-      console.log(res,"res==>");
-      this.availableTechData = res;
-     
-    })
-  }
-
-
-
-  logout(){
+  logout(): void{
     localStorage.removeItem('logindata')
   }
 
-  assignArtisan() {
+ 
 
-    setTimeout(function(){
-      window.location.reload();
-    }, 3000);
-   
-
-    this.message = "";
-      this.message = "Successfully assigned the request to a artisan, ";
-      return 
-      
-    
-  }
-
-  printer(){
+  printer(): void{
     window.print();
   }
 }
