43b9d3505ed47d2d0fbcaa32e2aedd3072784e94
unknown <neotshabalalamof5@gmail.com>
2023-06-09 11:28:41 +0200

    The assign task can now assign multiple technicians

--- src/app/adminassigntask/adminassigntask.component.ts
+++ src/app/adminassigntask/adminassigntask.component.ts
@@ -1,6 +1,5 @@
 import { Component,OnInit } from '@angular/core';
 import { ApiserviceService } from '../apiservice.service';
-import { Location } from '@angular/common';
 import { Route, Router } from '@angular/router';
 
 
@@ -18,11 +17,12 @@ export class AdminassigntaskComponent implements OnInit{
     tech_id:1,
     admin_id:12
   }
+  id:Number =1;
   object_:any;
   pdata:any;
   data:any;
   message = "";
-  status =true;
+  status =false;
   AdminArtisan_object: any;
 
   constructor(private service:ApiserviceService, private navrouter:Router){}
@@ -39,27 +39,22 @@ export class AdminassigntaskComponent implements OnInit{
   // data?.slice(10, 50)
 
  //Convert the local storage reference to a number
- let id = Number(data)
+this.id = Number(data)
 
   
   
 
 
  //Displaying the data
-  console.log(id, "REFENCE ID NUMBER")
+  console.log(this.id, "REFENCE ID NUMBER")
  
   console.log("NEW LINE")
     //Fetch the available artisan of the/based on the reference number.                or Number(data);
-    this.service.getTechAvailable(id).subscribe((res)=>{
+    this.service.getTechAvailable(this.id).subscribe((res)=>{
       console.log(res,"res==>");
       this.availableTechData = res;
 
-     
-
-      //Storing the Artisan details in the local storage
-
-      localStorage.setItem('techDetails',JSON.stringify(this.availableTechData))
-
+ 
       // let data = localStorage.getItem("techDetails");
 
       // // console.log(typeof(data), 'tHIS DATA');
@@ -73,30 +68,6 @@ export class AdminassigntaskComponent implements OnInit{
      
     })
 
-    // let requestReferenceData = localStorage.getItem('reference')
-
-    // //Convert to a number
-    // let refenceId = Number(requestReferenceData);
-
-    // console.log(refenceId,"reference");
-
-    
-    //Get the Artisan details from the local storage
-    let techdata = localStorage.getItem("techDetails");
-
-    //Slicing the string, in order to get the Arisan id
-     let artisan = techdata?.slice(12,20);
-    
-    //Convert to a number and storing it in the adminTechDetails Object
-    this.adminTechDetails.tech_id = Number(artisan);
-
-    console.log(this.adminTechDetails.tech_id, " Artisan Id nUMBER")
-   
-     
-    console.log("NEW LINE")
-
-    // console.log(this.adminTechDetails.tech_id,'ARTISAN NUMBER');
-    // console.log(typeof(staff_id))
 
 
       
@@ -105,52 +76,21 @@ export class AdminassigntaskComponent implements OnInit{
    
     let admin_temp_Id = admin?.slice(1,10);
     console.log(admin_temp_Id)
-  
-    // let admin_temp_Id = admin?.slice(0,1000);
-    // console.log(admin_temp_Id)
 
-     this.adminTechDetails.admin_id = Number(admin_temp_Id)
-     
-
-    console.log(this.adminTechDetails.admin_id , "Admin Id nuMBER")
-    console.log("NEW LINE")
-
-    //Convert the local storage admin id to a number
-    // this.adminTechDetails.admin_id= Number(admin);
-
-
-    // console.log(this.adminTechDetails.admin_id,"Admin id");
 
     //Displaying both staff number(of the Artisan&admin)
     console.log(this.adminTechDetails,"Display both of them")
 
 
 
-    this.service.assignavailArtisan(id,this.adminTechDetails).subscribe((res)=>{
-        this.AdminArtisan_object = res;
+    // this.service.assignavailArtisan(this.id,this.adminTechDetails).subscribe((res)=>{
+    //     this.AdminArtisan_object = res;
 
-      console.log(this.AdminArtisan_object )
+    //   console.log(this.AdminArtisan_object )
       
         
-    })
-    
-    //Display 
-    //Have to post the assigned Artisan status
-
-  //  this.service.assignavailArtisan(refenceId,this.status).subscribe((response)=>{
-
-        //   this.object_ = response;
-  
-
-
-  //   //   if(this.pdata.success=='true'){
-  //   //     console.log("Data submitted")
-       //   }
-      
-      
-      
-      
     // })
+    
 
 
 
@@ -165,30 +105,23 @@ export class AdminassigntaskComponent implements OnInit{
     localStorage.removeItem('logindata')
   }
 
-  assignArtisan() {
+  assignArtisan(tech_id:Number) {
 
-     setTimeout(function(){
-      window.location.reload();
-     }, 6000);
+     
    
     this.message = "";
       
 
-    this.message = "Successfully assigned the request to a artisan, ";
+    this.message = "Successfully assigned the request to the artisan"+tech_id;
 
-    let requestReferenceData = localStorage.getItem('reference')
 
-    let refenceId = Number(requestReferenceData);
 
-    console.log(refenceId,"reference");
-    //Get the Artisan details from the local storage
-    let techdata = localStorage.getItem("techDetails");
+   
+    //Get the Artisan id from the assignArtisan button(which is the data from the get available tech api), we only passsing and tech id from the html file 
+  
+     this.adminTechDetails.tech_id = Number(tech_id);
 
-    //Slicing the string, in order to get the Arisan id
-     let artisan = techdata?.slice(12,20);
-     
 
-     this.adminTechDetails.tech_id = Number(artisan);
    
      //Artisan staff number
     //console.log("25897486")
@@ -198,25 +131,31 @@ export class AdminassigntaskComponent implements OnInit{
 
 
       
-     //Getting the admin id in the local storage,the data is a string
-    let admin = localStorage.getItem('admin_id');
+    //  //Getting the admin id in the local storage,the data is a string
+    // let admin = localStorage.getItem('admin_id');
 
-    //Convert the local storage admin id to a number
-    this.adminTechDetails.admin_id= Number(admin);
+    // //Convert the local storage admin id to a number
+    // this.adminTechDetails.admin_id= Number(admin);
+    // console
 
 
-    console.log(this.adminTechDetails.admin_id,"Admin id");
-    //Have to post the assigned Artisan status
+    // console.log(this.adminTechDetails.admin_id,"Admin idCHECK THIS");
+    // //Have to post the assigned Artisan status
+  
+     this.service.assignavailArtisan(this.id,this.adminTechDetails).subscribe((res)=>{
 
-   this.service.assignavailArtisan(refenceId,this.adminTechDetails).subscribe((response)=>{
+        this.AdminArtisan_object = res;
 
-    this.object_ = response;
-    //   if(this.pdata.success=='true'){
-    //     console.log("Data submitted")
-    //   }
-   
+      console.log(this.AdminArtisan_object )
+      // setTimeout(function(){
+      //   window.location.reload();
+      //  }, 2000);
+      
+        
     })
-      return  
+      return  this.status =true
+
+     
   }
 
  
