308e2b22335aacbcd24c6ec43bdb07a5d29ac80b
unknown <neotshabalalamof5@gmail.com>
2023-07-25 10:01:38 +0200

    update

--- src/app/admincloselog/admincloselog.component.ts
+++ src/app/admincloselog/admincloselog.component.ts
@@ -19,12 +19,17 @@ export class AdmincloselogComponent {
   readData:any;
   tempdata:any
   ngOnInit():void{
-    this.service.waitingToBeClose().subscribe((res)=>{
-      this.tempdata = res
-      this.readData = this.tempdata.result
-      console.log(this.readData);
-    },(err)=>{
-      console.log(err)
+    // this.service.waitingToBeClose().subscribe((res)=>{
+    //   this.tempdata = res
+    //   this.readData = this.tempdata.result
+    //   console.log(this.readData);
+    // },(err)=>{
+    //   console.log(err)
+    // })
+    this.service.allRequests().subscribe((res)=>{
+      console.log(res.result,"res==>");
+      this.readData = res.result;
+      // localStorage.setItem('details', JSON.stringify(this.readData));
     })
 
 
